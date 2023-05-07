98daeeb2857fa4b4ad2e588db0e49ec406cf97b8
Armando Tavera <taveramex@gmail.com>
2023-05-07 15:09:51 -0600

    add two components for frontend
    test and push the new two components for the first phase.

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = (props)=>{
+
+    return( 
+        <navbar>
+            here's some options.
+            <ul>
+                <li>Browse mods</li>
+                <li>Create String Mod</li>
+                <li>Hello World</li>
+                <li>other option</li>
+            </ul>
+        </navbar>
+    );
+
+};
+
+export default Navbar;
