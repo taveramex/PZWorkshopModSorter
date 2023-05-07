2db126a30643a86c85e0cfbc1ab65f7b07523c50
Armando Tavera <taveramex@gmail.com>
2023-05-07 16:18:53 -0600

    Update Bootstrap Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import './App.css'
 import Navbar from './components/common/Navbar'
 import SpotLight from './components/Spotligth'
+import { Col, Container, Row } from 'react-bootstrap'
 
 function App() {
 
   return (
-    <>
-      <div>
-        <Navbar/>
-      </div>
-      <div>
-        <SpotLight/>
-      </div>
-    </>
+    <Container>
+      <Row>
+        <Col sm={2}><Navbar /></Col>
+        <Col sm={10}><SpotLight /></Col>
+      </Row>
+    </Container>
   )
 }
 
